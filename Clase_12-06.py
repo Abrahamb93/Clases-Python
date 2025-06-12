90e2f28273f97ac94283493d26bbbde14b7d036f
Abrahamb93 <abraham.balvoap@gmail.com>
2025-06-12 15:23:13 -0400

    Clase_12-06

---
+++ Clase_12-06.py
@@ -0,0 +1,66 @@
+# Uso y ejemplo de Lista
+
+#        -5 -4 -3  -2  -1
+numeros=[10, 5, 9, 34, 20]
+#        0   1  2  3   4
+
+# print(numeros[-1])
+#numeros.reverse()
+# for numero in numeros:
+#     #print(numero*3)
+#     print("Número", numero*3)
+
+# numeros.append(64) #append agrega un dato al final de la lista 
+# print(numeros)
+
+# frutas=["Durazno", "Naranja", "Manzana", "Pera"]
+
+# for fruta in frutas:
+#     print(fruta)
+
+# nombres=["Steven", "George"]
+# apellidos=["Carey", "Lewis"]
+# while True:
+#     # print('''
+#     # 1.- Ingresa un nombre y apellido
+#     # 2.- Buscar nombre
+#     # 3.- Mostrar nombres y apellidos 
+#     # 4.- Salir
+#     # ''')
+#     # op=int(input("Seleccione un opción: "))
+#     # match op:
+#     #     case 1:
+#     #         persona=input("Ingrese un nombre: ")
+#     #         nombres.append(persona)
+#     #         apell=input("Ingrese un apellido: ")
+#     #         apellidos.append(apell)
+#     #         #print(len(nombres))
+#     #     case 2: 
+#     #         nombre=input("Ingrese el nombre a buscar: ")
+#     #         if nombre in nombres: 
+#     #             print(f"El nombre {nombre} exite")
+#     #         else:
+#     #             print(f"El nombre {nombre} No exite")
+#     #     case 3: 
+#     #         # c=0
+#     #         # for n in nombres:
+#     #         #     print(nombres[c], apellidos[c])
+#     #         #     c+=1
+#     #         for i in range(len(nombres)):
+#     #             print(nombres[i], apellidos[i])
+#     #     case 4:
+#     #         print("Saliendo...")
+#     #         break
+#     #     case _:
+#     #         print("Opción no válida")
+
+'''
+Seleccione una opción
+1.- Agragar productos (nombre del producto y precio)
+2.- Comprar (submenu mostrando productos y precios)
+3.- Crear Boleta 
+4.- Salir
+productos []
+precios []
+carrito []
+''' 
